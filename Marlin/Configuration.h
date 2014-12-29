f4d0b79354e72936ac5991c01b831c2c48a50d72
dawhiz <rcollinson@gmail.com>
2014-12-29 03:59:00 -0700

    PID Settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -158,9 +158,9 @@
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 // Ultimaker
-    #define  DEFAULT_Kp 22.2
-    #define  DEFAULT_Ki 1.08
-    #define  DEFAULT_Kd 114
+    #define  DEFAULT_Kp 64.05
+    #define  DEFAULT_Ki 1.50
+    #define  DEFAULT_Kd 682.09
 
 // MakerGear
 //    #define  DEFAULT_Kp 7.0
